9d000d98b1c8fe1d403eb3b3995caaa619170bb5
Noellu <format941027@gmail.com>
2019-02-20 15:36:37 +0900

    핸들러 파일
    테스트용 핸들러 파일입니다

--- This_is_CatchMind/src/miniproject/catchmind/catchmind_Handler.java
+++ This_is_CatchMind/src/miniproject/catchmind/catchmind_Handler.java
@@ -1,12 +1,13 @@
 package miniproject.catchmind;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class catchmind_Handler {
+public class catchmind_Handler extends Thread{
 	private Socket socket;
 	private ArrayList<catchmind_Handler> list;
 	private ObjectOutputStream oos;
@@ -19,9 +20,45 @@ public class catchmind_Handler {
 		oos = new ObjectOutputStream(socket.getOutputStream());
 		ois = new ObjectInputStream(socket.getInputStream());
 	}
-
-	public static void main(String[] args) {
-		new catchmind_Server();
+	
+	@Override
+	public void run() {
+		catchmind_ShapDTO dto = null;
+		
+		while(true) {
+			try {
+				dto = (catchmind_ShapDTO)ois.readObject();
+				
+				catchmind_ShapDTO sendDTO = new catchmind_ShapDTO();
+				sendDTO.setX1(dto.getX1());
+				sendDTO.setY1(dto.getY1());
+				sendDTO.setX2(dto.getX2());
+				sendDTO.setY2(dto.getY2());
+				
+				sendDTO.setColorNum(dto.getColorNum());
+				
+				sendDTO.setShape(dto.getShape());
+				
+				broadcast(sendDTO);
+				
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void broadcast(catchmind_ShapDTO sendDTO) {
+		for(catchmind_Handler handler : list) {
+			try {
+				handler.oos.writeObject(sendDTO);
+				handler.oos.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
 	}
 
 }
